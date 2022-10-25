39593f3c5f2675e2e12ce78c5dadc92d8a0027e3
minseokk <minseokk@postech.ac.kr>
2022-10-25 22:27:48 +0900

    v.0.0.1

---
+++ README.md
@@ -0,0 +1,5 @@
+# USAGE
+1. `poetry install`
+2. put images to resize in the `input` folder 
+3. `poetry run python main.py`
+4. resized image will be generated in the `output` folder
---
+++ main.py
@@ -0,0 +1,20 @@
+from PIL import Image
+
+from resizeimage import resizeimage
+
+import os
+
+cur_path = os.path.dirname(__file__)
+
+new_path = os.path.relpath('./input/', cur_path)
+
+for filename in os.listdir(new_path):
+    print(filename)
+    path_filename = './input/' + filename
+
+    with open(path_filename, 'r+b') as f:
+        with Image.open(f) as image:
+            cover = resizeimage.resize_width(image, 500)
+            new_filename = "./output/sm-" + filename
+            print(new_filename)
+            cover.save(new_filename, image.format)
