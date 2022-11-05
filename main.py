c87f5ebeb40747a68b3016f9c8d604efc7c7faee
minseokk <minseokk@postech.ac.kr>
2022-11-05 11:57:53 +0900

    name changing

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ cur_path = os.path.dirname(__file__)
 
 new_path = os.path.relpath('./input/', cur_path)
 
+artist = "yoonji"
+count = 1
+
 for filename in os.listdir(new_path):
     print(filename)
     path_filename = './input/' + filename
@@ -15,6 +18,7 @@ for filename in os.listdir(new_path):
     with open(path_filename, 'r+b') as f:
         with Image.open(f) as image:
             cover = resizeimage.resize_width(image, 500)
-            new_filename = "./output/sm-" + filename
+            new_filename = "./output/sm-" + artist + str(count) + os.path.splitext(filename)[1]
             print(new_filename)
             cover.save(new_filename, image.format)
+            count += 1
